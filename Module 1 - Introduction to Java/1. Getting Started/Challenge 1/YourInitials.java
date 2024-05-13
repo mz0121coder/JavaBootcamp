694f4050c8b4ade0b275def2b026f4584cb8c497
Mohammad Zafar <mzcoder7@gmail.com>
2024-05-13 15:27:06 +0100

    Add files with my solutions up to workbook 3

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("M   M  ZZZZZ");
+        System.out.println("MM MM      Z");
+        System.out.println("M M M     Z ");
+        System.out.println("M   M    Z  ");
+        System.out.println("M   M   Z   ");
+        System.out.println("M   M  Z    ");
+        System.out.println("M   M  ZZZZZ");
+    }
+}
---
+++ Module 1 - Introduction to Java/2. Variables/Section Two/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+  public static void main(String[] args) {
+    char potions = 'A';
+    char arithmancy = 'B';
+    char charms = 'c';
+    
+    System.out.println("Potions " + potions);
+    System.out.println("Arithmancy " + arithmancy);
+    System.out.println("Charms " + charms);
+  }
+}
